6d4b4f534eeeae7cc09e65b6186ecc4915c73f0a
Chinmoy Raj Deka <rajchinmoy007@gmail.com>
2023-12-26 15:59:47 +0530

    toolbar modified

--- src/controlls/tool-bar.ts
+++ src/controlls/tool-bar.ts
@@ -8,12 +8,14 @@ import FullSrcCtrl from "./full-src-button";
 class Toolbar implements Controll {
   private _visibility: boolean = false;
   private _el: HTMLElement;
+  private _backMain: HTMLElement;
+  private _titleMain: HTMLElement;
+  private _fullScrMain: HTMLElement;
   private _backBtn: HTMLElement;
   private _title: HTMLParagraphElement
   constructor(fullscrCtrl: FullSrcCtrl, config: PlayerConfig) {
     this._el = document.createElement('div');
     this._el.className = styles.toolBar;
-    if (!config.showVideoTitle) this._el.style.display = "none";
     this._backBtn = createCtrl({ icon: Material.ArrowBackIcon });
     this._backBtn.addEventListener('click', () => {
       history.back();
@@ -21,7 +23,17 @@ class Toolbar implements Controll {
     this._title = document.createElement('p');
     this._title.className = styles.title;
     this._title.textContent = config.title;
-    this._el.append(this._backBtn, this._title, fullscrCtrl.element);
+    this._backMain = document.createElement('div');
+    this._titleMain = document.createElement('div');
+    this._fullScrMain = document.createElement('div');
+    if (!config.showVideoTitle) this._titleMain.style.display = "none";
+    this._backMain.className = styles.backArrowMainDiv;
+    this._backMain.append(this._backBtn);
+    this._titleMain.className = styles.titleMainDiv;
+    this._titleMain.append(this._title);
+    this._fullScrMain.className = styles.fullScrMainDiv;
+    this._fullScrMain.append(fullscrCtrl.element);
+    this._el.append(this._backMain, this._titleMain, this._fullScrMain);
   }
 
   public show() {
